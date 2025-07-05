242eb5ee4fc82a34e16730075ef576d02159136b
LNikita19 <nikita.lilhore@pinnacle.in>
2025-07-05 22:31:46 +0530

    ++config

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-export const API_BASE_URL = "https://server-six-beryl.vercel.app";
+export const API_BASE_URL = "https://server-9ao7.vercel.app";
 // export const API_BASE_URL = "http://localhost:4000";
 
